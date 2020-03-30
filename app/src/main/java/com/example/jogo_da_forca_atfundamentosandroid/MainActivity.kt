e15e5432a199993602d22ed04dd515afbe0622fd
ThamyresAbal <thamyresabal@gmail.com>
2020-03-29 22:47:25 -0300

    Lógica do jogo completa

--- app/src/main/java/com/example/jogo_da_forca_atfundamentosandroid/MainActivity.kt
+++ app/src/main/java/com/example/jogo_da_forca_atfundamentosandroid/MainActivity.kt
@@ -1,55 +1,105 @@
 package com.example.jogo_da_forca_atfundamentosandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProviders
+import androidx.appcompat.app.AppCompatActivity
 import com.example.jogo_da_forca_atfundamentosandroid.Model.lista
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
-import android.widget.EditText as EditText1
 
 class MainActivity : AppCompatActivity() {
+
+     lateinit var listaPalavrasAcertadas: MutableList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      //  var VM = ViewModelProviders.of(this).get(PalavrasViewModel::class.java)
+        //var VM = ViewModelProviders.of(this).get(PalavrasViewModel::class.java)
+
+        //falta ver lance o enter
+        //falta ver proxima palavra
 
         //exibe um elemento aleatorio da lista
-        val texto =  lista.random().toUpperCase()
-        textViewTeste.text = texto
-        button.setOnClickListener(){
+        var texto =  lista.random().toUpperCase()
+        var tentativas = 5
+        var textoOculto = texto
+        var caracteresCertos = "" //concatenar cada caracter que estiver certo
+        var letrasUsadas = ""
+
+        for (caracter in texto){
+            textoOculto = textoOculto.replace(caracter.toString(), "_ ", true)
+        }
+        textViewPalavraDaVez.text = textoOculto
+
+
+        button.setOnClickListener() {
             //Ao clicar no botão o método onClick será executado.
-            val letraPorLetra  = editTextLetra.text.toString()
-            val tentativas = 5
-                if (letraPorLetra.length == 1) {
+            val letraPorLetra = editTextLetra.text.toString().toUpperCase()
 
-                    while (tentativas == 0) {
+            if (letrasUsadas.contains(letraPorLetra)) {
+                Toast.makeText(this,"Você já usou esta letra", Toast.LENGTH_SHORT).show()
+            }else {
+                letrasUsadas += letraPorLetra
+                textViewLetrasUsadas.text = letrasUsadas
+                if (tentativas > 0) {
+                    if (letraPorLetra.length == 1) {
                         if (texto.contains(letraPorLetra)) {
-                            textViewPalavraDaVez.text = letraPorLetra
+                            caracteresCertos += letraPorLetra
+                            textoOculto =
+                                texto //Reiniciar a palavra para reiniciar a verificação dos caracteres
+                            //Percorrendo verificando quais caracteres acertou
+                            for (caracter in texto) {
+                                //remontando a palavra do jogo mostrando os caracteres certos e ocultando
+                                //aqueles que ainda não acertaram
+                                if (caracteresCertos.contains(caracter.toString(), true)) {
+                                    //Verificar para mostrar somente as letras certas
+                                    //acertou a letra
+                                    textoOculto = textoOculto.replace(
+                                        caracter.toString(),
+                                        caracter.toString(),
+                                        true
+                                    )
+                                    Toast.makeText(this, "Parabéns!", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    //errou
+                                    //Ocultar os outros caracteres
+                                    textoOculto =
+                                        textoOculto.replace(caracter.toString(), "_ ", true)
+                                }
+                            }
+                            if (!textoOculto.contains("_")) {
+                                Toast.makeText(
+                                    this,
+                                    "Acertou! Vamos a próxima palavra.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                listaPalavrasAcertadas.add(texto)
+                                 texto = lista.random().toUpperCase()
+                            }
+                            textViewPalavraDaVez.text = textoOculto
+                            editTextLetra.text.clear()
 
-                            var textoDigitado = editTextLetra.text.toString()
-                            textViewoioi.text = textoDigitado
-
-                            tentativas - 1
                         } else {
+                            tentativas -= 1
+                            if (tentativas == 0) {
+                                textViewPalavraDaVez.text = texto
+                                textViewErro.text = "Fim de jogo!"
+                            }
+                            editTextLetra.text.clear()
                             Toast.makeText(
                                 this,
                                 "Errou! Você tem $tentativas",
-                                Toast.LENGTH_LONG
+                                Toast.LENGTH_SHORT
                             ).show()
                         }
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Apenas uma letra por vez!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
-                } else {
-                    Toast.makeText(
-                        this,
-                        "Apenas uma letra por vez!",
-                        Toast.LENGTH_LONG
-                    ).show()
+                }
             }
         }
     }
